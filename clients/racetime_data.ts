fd17c8774f3efb2662ce2c317c2947bc360f1124
Tobias Gies <tobias@tobiasgies.de>
2024-01-23 23:29:37 +0100

    Add tinyduration + comparable duration extension

--- clients/racetime_data.ts
+++ clients/racetime_data.ts
@@ -1,3 +1,6 @@
+import {Duration} from "tinyduration";
+import {ComparableDuration, parse} from "../data/comparable_duration";
+
 export class RacetimeUser {
     readonly id: string;
     readonly full_name: string;
@@ -15,21 +18,40 @@ export class RacetimeUser {
     readonly teams: RacetimeTeam[] | null;
     readonly races: RacetimeRace[] | null;
 
-    constructor(id: string,
-                full_name: string,
-                name: string,
-                discriminator: string | null,
-                url: string,
-                avatar: string,
-                pronouns: string,
-                flair: string,
-                twitch_name: string,
-                twitch_display_name: string,
-                twitch_channel: string,
-                can_moderate: boolean,
-                stats: RacetimeUserStats | null = null,
-                teams: RacetimeTeam[] | null = null,
-                races: RacetimeRace[] | null = null) {
+    constructor(
+        {
+            id,
+            full_name,
+            name,
+            discriminator,
+            url,
+            avatar,
+            pronouns,
+            flair,
+            twitch_name,
+            twitch_display_name,
+            twitch_channel,
+            can_moderate,
+            stats = null,
+            teams = null,
+            races = null
+        }: {
+            id: string,
+            full_name: string,
+            name: string,
+            discriminator: string | null,
+            url: string,
+            avatar: string,
+            pronouns: string,
+            flair: string,
+            twitch_name: string,
+            twitch_display_name: string,
+            twitch_channel: string,
+            can_moderate: boolean,
+            stats?: RacetimeUserStats | null,
+            teams?: RacetimeTeam[] | null,
+            races?: RacetimeRace[] | null
+        }) {
         this.id = id;
         this.full_name = full_name;
         this.name = name;
@@ -55,7 +77,21 @@ export class RacetimeUserStats {
     readonly third: number;
     readonly forfeits: number;
 
-    constructor(joined: number, first: number, second: number, third: number, forfeits: number) {
+    constructor(
+        {
+            joined,
+            first,
+            second,
+            third,
+            forfeits
+        }: {
+            joined: number,
+            first: number,
+            second: number,
+            third: number,
+            forfeits: number
+        }
+    ) {
         this.joined = joined;
         this.first = first;
         this.second = second;
@@ -75,15 +111,29 @@ export class RacetimeTeam {
     readonly categories: RacetimeCategory[] | null;
     readonly members: RacetimeUser[] | null;
 
-    constructor(name: string,
-                slug: string,
-                formal: boolean,
-                url: string,
-                data_url: string,
-                avatar: string,
-                profile: string,
-                categories: RacetimeCategory[] | null = null,
-                members: RacetimeUser[] | null = null) {
+    constructor(
+        {
+            name,
+            slug,
+            formal,
+            url,
+            data_url,
+            avatar,
+            profile,
+            categories = null,
+            members = null
+        }: {
+            name: string,
+            slug: string,
+            formal: boolean,
+            url: string,
+            data_url: string,
+            avatar: string,
+            profile: string,
+            categories?: RacetimeCategory[] | null,
+            members?: RacetimeUser[] | null
+        }
+    ) {
         this.name = name;
         this.slug = slug;
         this.formal = formal;
@@ -101,7 +151,17 @@ export class RacetimeRaceStatus {
     readonly verbose_value: string;
     readonly help_text: string;
 
-    constructor(value: string, verbose_value: string, help_text: string) {
+    constructor(
+        {
+            value,
+            verbose_value,
+            help_text
+        }: {
+            value: string,
+            verbose_value: string,
+            help_text: string
+        }
+    ) {
         this.value = value;
         this.verbose_value = verbose_value;
         this.help_text = help_text;
@@ -112,7 +172,15 @@ class RacetimeRaceGoal {
     readonly name: string;
     readonly custom: boolean;
 
-    constructor(name: string, custom: boolean) {
+    constructor(
+        {
+            name,
+            custom
+        }: {
+            name: string,
+            custom: boolean
+        }
+    ) {
         this.name = name;
         this.custom = custom;
     }
@@ -123,7 +191,17 @@ class RacetimeEntrantStatus {
     readonly verbose_value: string;
     readonly help_text: string;
 
-    constructor(value: string, verbose_value: string, help_text: string) {
+    constructor(
+        {
+            value,
+            verbose_value,
+            help_text
+        }: {
+            value: string,
+            verbose_value: string,
+            help_text: string
+        }
+    ) {
         this.value = value;
         this.verbose_value = verbose_value;
         this.help_text = help_text;
@@ -134,7 +212,7 @@ class RacetimeEntrant {
     readonly user: RacetimeUser;
     readonly team: RacetimeTeam | null;
     readonly status: RacetimeEntrantStatus;
-    readonly finish_time: string | null;
+    readonly finish_time: ComparableDuration | null;
     readonly finished_at: Date | null;
     readonly place: number | null;
     readonly place_ordinal: string | null;
@@ -145,23 +223,41 @@ class RacetimeEntrant {
     readonly stream_live: boolean;
     readonly stream_override: boolean;
 
-    constructor(user: RacetimeUser,
-                status: RacetimeEntrantStatus,
-                stream_live: boolean,
-                stream_override: boolean,
-                has_comment: boolean,
-                comment: string | null = null,
-                team: RacetimeTeam | null = null,
-                score: number | null = null,
-                score_change: number | null = null,
-                finish_time: string | null = null,
-                finished_at: Date | null = null,
-                place: number | null = null,
-                place_ordinal: string | null = null) {
+    constructor(
+        {
+            user,
+            status,
+            stream_live,
+            stream_override,
+            has_comment,
+            comment = null,
+            team = null,
+            score = null,
+            score_change = null,
+            finish_time = null,
+            finished_at = null,
+            place = null,
+            place_ordinal = null
+        }: {
+            user: RacetimeUser,
+            status: RacetimeEntrantStatus,
+            stream_live: boolean,
+            stream_override: boolean,
+            has_comment: boolean,
+            comment?: string | null,
+            team?: RacetimeTeam | null,
+            score?: number | null,
+            score_change?: number | null,
+            finish_time?: string | null,
+            finished_at?: Date | null,
+            place?: number | null,
+            place_ordinal?: string | null
+        }
+    ) {
         this.user = user;
         this.team = team;
         this.status = status;
-        this.finish_time = finish_time;
+        this.finish_time = !!finish_time ? parse(finish_time) : null;
         this.finished_at = finished_at;
         this.place = place;
         this.place_ordinal = place_ordinal;
@@ -189,19 +285,37 @@ class RacetimeCategory {
     readonly current_races: RacetimeRace[] | null;
     readonly emotes: {[name: string]: string} | null;
 
-    constructor(name: string,
-                short_name: string,
-                slug: string,
-                url: string,
-                data_url: string,
-                image: string,
-                info: string | null = null,
-                streaming_required: boolean | null = null,
-                owners: RacetimeUser[] | null = null,
-                moderators: RacetimeUser[] | null = null,
-                goals: string[] | null = null,
-                current_races: RacetimeRace[] | null = null,
-                emotes: { [name: string]: string } | null = null) {
+    constructor(
+        {
+            name,
+            short_name,
+            slug,
+            url,
+            data_url,
+            image,
+            info = null,
+            streaming_required = null,
+            owners = null,
+            moderators = null,
+            goals = null,
+            current_races = null,
+            emotes = null
+        }: {
+            name: string,
+            short_name: string,
+            slug: string,
+            url: string,
+            data_url: string,
+            image: string,
+            info?: string | null,
+            streaming_required?: boolean | null,
+            owners?: RacetimeUser[] | null,
+            moderators?: RacetimeUser[] | null,
+            goals?: string[] | null,
+            current_races?: RacetimeRace[] | null,
+            emotes?: { [p: string]: string } | null
+        }
+    ) {
         this.name = name;
         this.short_name = short_name;
         this.slug = slug;
@@ -239,12 +353,12 @@ export class RacetimeRace {
     readonly entrants_count_inactive: number;
     readonly entrants: RacetimeEntrant[] | null;
     readonly opened_at: Date;
-    readonly start_delay: string | null; // TODO convert to duration
+    readonly start_delay: Duration | null;
     readonly started_at: Date | null;
     readonly ended_at: Date | null;
     readonly cancelled_at: Date | null;
     readonly unlisted: boolean | null;
-    readonly time_limit: string; // TODO convert to duration
+    readonly time_limit: Duration;
     readonly time_limit_auto_complete: boolean | null;
     readonly require_even_teams: boolean | null;
     readonly streaming_required: boolean | null;
@@ -259,49 +373,95 @@ export class RacetimeRace {
     readonly allow_prerace_chat: boolean | null;
     readonly allow_midrace_chat: boolean | null;
     readonly allow_non_entrant_chat: boolean | null;
-    readonly chat_message_delay: string | null; // TODO convert to duration
+    readonly chat_message_delay: Duration | null;
 
-    constructor(name: string,
-                status: RacetimeRaceStatus,
-                url: string,
-                data_url: string,
-                goal: RacetimeRaceGoal,
-                info: string,
-                entrants_count: number,
-                entrants_count_finished: number,
-                entrants_count_inactive: number,
-                opened_at: Date,
-                time_limit: string,
-                version: number | null = null,
-                slug: string | null = null,
-                websocket_url: string | null = null,
-                websocket_bot_url: string | null = null,
-                websocket_oauth_url: string | null = null,
-                category: RacetimeCategory | null = null,
-                info_bot: string | null = null,
-                info_user: string | null = null,
-                team_race: boolean | null = null,
-                entrants: RacetimeEntrant[] | null = null,
-                start_delay: string | null = null,
-                started_at: Date | null = null,
-                ended_at: Date | null = null,
-                cancelled_at: Date | null = null,
-                unlisted: boolean | null = null,
-                time_limit_auto_complete: boolean | null = null,
-                require_even_teams: boolean | null = null,
-                streaming_required: boolean | null = null,
-                auto_start: boolean | null = null,
-                opened_by: RacetimeUser | null = null,
-                monitors: RacetimeUser[] | null = null,
-                recordable: boolean | null = null,
-                recorded: boolean | null = null,
-                recorded_by: RacetimeUser | null = null,
-                allow_comments: boolean | null = null,
-                hide_comments: boolean | null = null,
-                allow_prerace_chat: boolean | null = null,
-                allow_midrace_chat: boolean | null = null,
-                allow_non_entrant_chat: boolean | null = null,
-                chat_message_delay: string | null = null) {
+    constructor(
+        {
+            name,
+            status,
+            url,
+            data_url,
+            goal,
+            info,
+            entrants_count,
+            entrants_count_finished,
+            entrants_count_inactive,
+            opened_at,
+            time_limit,
+            version = null,
+            slug = null,
+            websocket_url = null,
+            websocket_bot_url = null,
+            websocket_oauth_url = null,
+            category = null,
+            info_bot = null,
+            info_user = null,
+            team_race = null,
+            entrants = null,
+            start_delay = null,
+            started_at = null,
+            ended_at = null,
+            cancelled_at = null,
+            unlisted = null,
+            time_limit_auto_complete = null,
+            require_even_teams = null,
+            streaming_required = null,
+            auto_start = null,
+            opened_by = null,
+            monitors = null,
+            recordable = null,
+            recorded = null,
+            recorded_by = null,
+            allow_comments = null,
+            hide_comments = null,
+            allow_prerace_chat = null,
+            allow_midrace_chat = null,
+            allow_non_entrant_chat = null,
+            chat_message_delay = null
+        }: {
+            name: string,
+            status: RacetimeRaceStatus,
+            url: string,
+            data_url: string,
+            goal: RacetimeRaceGoal,
+            info: string,
+            entrants_count: number,
+            entrants_count_finished: number,
+            entrants_count_inactive: number,
+            opened_at: Date,
+            time_limit: string,
+            version?: number | null,
+            slug?: string | null,
+            websocket_url?: string | null,
+            websocket_bot_url?: string | null,
+            websocket_oauth_url?: string | null,
+            category?: RacetimeCategory | null,
+            info_bot?: string | null,
+            info_user?: string | null,
+            team_race?: boolean | null,
+            entrants?: RacetimeEntrant[] | null,
+            start_delay?: string | null,
+            started_at?: Date | null,
+            ended_at?: Date | null,
+            cancelled_at?: Date | null,
+            unlisted?: boolean | null,
+            time_limit_auto_complete?: boolean | null,
+            require_even_teams?: boolean | null,
+            streaming_required?: boolean | null,
+            auto_start?: boolean | null,
+            opened_by?: RacetimeUser | null,
+            monitors?: RacetimeUser[] | null,
+            recordable?: boolean | null,
+            recorded?: boolean | null,
+            recorded_by?: RacetimeUser | null,
+            allow_comments?: boolean | null,
+            hide_comments?: boolean | null,
+            allow_prerace_chat?: boolean | null,
+            allow_midrace_chat?: boolean | null,
+            allow_non_entrant_chat?: boolean | null,
+            chat_message_delay?: string | null
+        }
+    ) {
         this.version = version;
         this.name = name;
         this.slug = slug;
@@ -322,12 +482,12 @@ export class RacetimeRace {
         this.entrants_count_inactive = entrants_count_inactive;
         this.entrants = entrants;
         this.opened_at = opened_at;
-        this.start_delay = start_delay;
+        this.start_delay = !!start_delay ? parse(start_delay) : null;
         this.started_at = started_at;
         this.ended_at = ended_at;
         this.cancelled_at = cancelled_at;
         this.unlisted = unlisted;
-        this.time_limit = time_limit;
+        this.time_limit = !!time_limit ? parse(time_limit) : null;
         this.time_limit_auto_complete = time_limit_auto_complete;
         this.require_even_teams = require_even_teams;
         this.streaming_required = streaming_required;
@@ -342,6 +502,6 @@ export class RacetimeRace {
         this.allow_prerace_chat = allow_prerace_chat;
         this.allow_midrace_chat = allow_midrace_chat;
         this.allow_non_entrant_chat = allow_non_entrant_chat;
-        this.chat_message_delay = chat_message_delay;
+        this.chat_message_delay = !!chat_message_delay ? parse(chat_message_delay) : null;
     }
 }
