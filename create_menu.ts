3177c3425d02c1a835aacaba23b3bf8b3ce3a259
Tobias Gies <tobias.gies@alumni.fh-aachen.de>
2024-02-06 20:01:56 +0100

    Use spreadsheet menu instead of SheetsApp UI menu

--- create_menu.ts
+++ create_menu.ts
@@ -1,11 +1,10 @@
 import SheetsOnOpen = GoogleAppsScript.Events.SheetsOnOpen;
 
 function onOpen(e: SheetsOnOpen): void {
-    const ui = SpreadsheetApp.getUi();
-    ui.createMenu('Restream')
-        .addItem('Generate S7CC Layout', 'AutoLayout')
-        .addItem('Generate Generic Layout', 'AutoLayout') // TODO separate function targeting generic layout
-        .addToUi();
+    e.source.addMenu("Restream", [
+        { name: "Generate S7CC Layout", functionName: "AutoLayout" },
+        { name: "Generate Generic Layout", functionName: "AutoLayout" } // TODO separate function targeting generic layout
+    ])
 }
 
 
