371521d16d781319a64d10535f53e83ebcf17e74
Tobias Gies <tobias@tobiasgies.de>
2024-02-13 01:39:16 +0100

    Convert player stats best time to Duration type

--- data/player_stats.ts
+++ data/player_stats.ts
@@ -1,16 +1,17 @@
 import {Player} from "./player";
+import {Duration} from "tinyduration";
 
 export class PlayerStats {
     readonly player: Player;
     readonly numberOfRaces: number;
-    readonly bestTime: string; // TODO convert to duration type
+    readonly bestTime: Duration;
     readonly bestTimeDate: Date;
     readonly numberOfWins: number;
     readonly numberOfSeconds: number;
     readonly numberOfThirds: number;
     readonly numberOfForfeits: number;
 
-    constructor(player: Player, numberOfRaces: number, bestTime: string, bestTimeDate: Date, numberOfWins: number, numberOfSeconds: number, numberOfThirds: number, numberOfForfeits: number) {
+    constructor(player: Player, numberOfRaces: number, bestTime: Duration, bestTimeDate: Date, numberOfWins: number, numberOfSeconds: number, numberOfThirds: number, numberOfForfeits: number) {
         this.player = player;
         this.numberOfRaces = numberOfRaces;
         this.bestTime = bestTime;
