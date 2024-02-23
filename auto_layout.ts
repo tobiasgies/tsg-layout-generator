edd1790e042c18891b96a5e6c751b367d3504c6d
Tobias Gies <tobias@tobiasgies.de>
2024-02-23 23:55:56 +0100

    We have actual select boxes now.

--- auto_layout.ts
+++ auto_layout.ts
@@ -5,73 +5,85 @@ import {Racetime} from "./clients/racetime";
 import {MidosHouse} from "./clients/midos_house";
 import {ChallengeCupSeason7} from "./slide_decks/ccs7";
 import {Race} from "./clients/racetime_data";
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
 
 const midos = new MidosHouse()
+
+/**
+ * Display a select dialog to start generating a restream layout for CCS7 races. Triggered from menu interaction.
+ */
+function selectCCS7Race() {
+    try {
+        const tab = getCCS7Tab()
+        const scheduledRaces = getCCS7ScheduledRaces(tab)
+        let template = HtmlService.createTemplateFromFile("select_race.tpl")
+        template.races = scheduledRaces
+        SpreadsheetApp.getUi().showModelessDialog(template.evaluate(), "Select Race")
+    } catch (e) {
+        SpreadsheetApp.getUi().alert(`An error has occurred: ${e.message}`)
+        console.error(e)
+    }
+
+}
+
+function getCCS7Tab() {
+    const sheetsTabId = parseInt(PropertiesService.getScriptProperties().getProperty("CCS7_TAB_ID"));
+    let tab = SpreadsheetApp.getActiveSpreadsheet().getSheets().find(it => it.getSheetId() == sheetsTabId)
+    if (!tab) {
+        throw new Error(`Could not find Google Sheets tab that contains race data. Tab ID ${sheetsTabId} not found.`)
+    }
+    return tab;
+}
+
+function getCCS7ScheduledRaces(tab: Sheet) {
+    return tab.getRange(`A2:N300`).getValues()
+        .filter(row => !!row[12])
+        .map(row => new ScheduledRace(
+            row[12], row[0], row[1], row[2], row[3], row[4], row[5],
+            row[6],row[7], row[8], row[9], row[10], row[11], row[13]
+        ));
+}
+
+function getCCS7Presentation() {
+    const presentationId = PropertiesService.getScriptProperties().getProperty("CCS7_PRESENTATION_ID");
+    const presentation = SlidesApp.openById(presentationId);
+    if (!presentation) {
+        throw new Error(`Could not find Google Slides presentation containing layout. Presentation ${presentationId} not found.`)
+    }
+    return presentation;
+}
 
 function filterCCS7Races(race: Race): boolean {
     return (race.goal.custom == false && race.goal.name == "Standard Ruleset") ||
         (race.goal.custom == true && midos.isStandardGoal(race.goal.name))
 }
 
-function layoutCCS7(): void {
-    const ui = SpreadsheetApp.getUi();
-
-    const sheetsTabId = parseInt(PropertiesService.getScriptProperties().getProperty("CCS7_TAB_ID"));
-    const tab = SpreadsheetApp.getActiveSpreadsheet().getSheets().find(it => it.getSheetId() == sheetsTabId)
-    if (!tab) {
-        ui.alert(`ERROR: Could not find Google Sheets tab that contains race data. Tab ID ${sheetsTabId} not found.`)
-        return;
+/**
+ * Generate CCS7 layout for given race ID and return URL of presentation. Triggered from client-side javascript.
+ * @param raceId The race ID to generate a layout for
+ * @return The generated Google Slides' URL
+ */
+function layoutCCS7(raceId: String): string {
+    const tab = getCCS7Tab();
+    const presentation = getCCS7Presentation();
+    const scheduledRace = getCCS7ScheduledRaces(tab).find(race => race.raceId == raceId)
+    if (!scheduledRace) {
+        throw new Error(`Could not find race with Race ID ${raceId}.`)
     }
-
-    const presentationId = PropertiesService.getScriptProperties().getProperty("CCS7_PRESENTATION_ID");
-    const presentation = SlidesApp.openById(presentationId);
-    if (!presentation) {
-        ui.alert(`ERROR: Could not find Google Slides presentation containing layout. Presentation ${presentationId} not found.`)
-        return;
-    }
-
-    let rowNumber = ui.prompt("Enter the row number of the match").getResponseText();
-    if (isNaN(parseInt(rowNumber))) {
-        ui.alert("Invalid input, please enter a number");
-        return;
-    }
-
-    let row = tab.getRange(`A${rowNumber}:AH${rowNumber}`).getValues()[0];
-    if (!row[12]) {
-        ui.alert("You seem to have selected an empty row - the race ID is not set.")
-        return;
-    }
-
-    let scheduledRace = new ScheduledRace(
-        row[12], row[0], row[1], row[2], row[3], row[4], row[5],
-        row[6],row[7], row[8], row[9], row[10], row[11], row[13]
-    )
 
     // Fetch runner stats from Racetime and calculate face-off stats
-    try {
-        const racetime = new Racetime()
-        const user1 = racetime.fetchUser(scheduledRace.runner1RacetimeId);
-        const user2 = racetime.fetchUser(scheduledRace.runner2RacetimeId);
-        const player1 = new Player(scheduledRace.runner1Name, user1.twitch_name, scheduledRace.runner1QualifierRank, scheduledRace.runner1Country, user1.id, user1.pronouns);
-        const player2 = new Player(scheduledRace.runner2Name, user2.twitch_name, scheduledRace.runner2QualifierRank, scheduledRace.runner2Country, user2.id, user2.pronouns);
-        const races: Race[] = [user1, user2].flatMap(racetime.fetchUserRaces.bind(racetime));
-        const racesDedup = [... new Set(races)];
-        const stats = FaceOffStats.fromRacetime(racesDedup, player1, player2, filterCCS7Races);
-        const ccs7 = new ChallengeCupSeason7(presentation);
-        ccs7.layoutTitleSlide(player1, player2, scheduledRace.round);
-        ccs7.layoutStatsSlide(stats);
-        ccs7.layoutRaceSlide(player1, player2, scheduledRace.round);
+    const racetime = new Racetime();
+    const user1 = racetime.fetchUser(scheduledRace.runner1RacetimeId);
+    const user2 = racetime.fetchUser(scheduledRace.runner2RacetimeId);
+    const player1 = new Player(scheduledRace.runner1Name, user1.twitch_name, scheduledRace.runner1QualifierRank, scheduledRace.runner1Country, user1.id, user1.pronouns);
+    const player2 = new Player(scheduledRace.runner2Name, user2.twitch_name, scheduledRace.runner2QualifierRank, scheduledRace.runner2Country, user2.id, user2.pronouns);
+    const races: Race[] = [user1, user2].flatMap(racetime.fetchUserRaces.bind(racetime));
+    const racesDedup = [... new Set(races)];
+    const stats = FaceOffStats.fromRacetime(racesDedup, player1, player2, filterCCS7Races);
+    const ccs7 = new ChallengeCupSeason7(presentation);
+    ccs7.layoutTitleSlide(player1, player2, scheduledRace.round);
+    ccs7.layoutStatsSlide(stats);
+    ccs7.layoutRaceSlide(player1, player2, scheduledRace.round);
 
-        const link = ccs7.getPresentationLink();
-        SpreadsheetApp.getUi().showModelessDialog(HtmlService.createHtmlOutput(
-            '<script>' +
-                'window.open("' + link + '", "_blank");' +
-                'window.setTimeout(() => google.script.host.close(), 15000);' +
-            '</script>' +
-            '<p>Attempting to open generated slides. If this does not work, please <a href="' + link +'">click here.</a></p>'
-        ), 'Opening...');
-    } catch (e) {
-        ui.alert(`Sorry, but an error occured fetching data from racetime.gg: ${e.message}`)
-        throw e;
-    }
+    return ccs7.getPresentationLink();
 }
--- create_menu.ts
+++ create_menu.ts
@@ -2,7 +2,7 @@ import SheetsOnOpen = GoogleAppsScript.Events.SheetsOnOpen;
 
 function onOpen(e: SheetsOnOpen): void {
     e.source.addMenu("Restream", [
-        { name: "Generate S7CC Layout", functionName: "layoutCCS7" }
+        { name: "Generate S7CC Layout", functionName: "selectCCS7Race" }
     ])
 }
 
